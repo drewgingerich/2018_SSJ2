92bcfa866f6e28711b355b06f2b44cefd112ecb3
Drew Gingerich <gingerich93@gmail.com>
2018-07-20 22:51:38 -0700

    Add script to let player move camera left and right.

---
+++ Assets/scripts/StrafeControls.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrafeControls : MonoBehaviour {
+
+	[SerializeField] float speed = 1;
+	[SerializeField] float maxDistance = 3;
+	
+	void Update () {
+		float input = Input.GetAxisRaw("Horizontal");
+		float move = input * speed * Time.deltaTime;
+		Vector3 newPosition = transform.position + new Vector3(move, 0, 0);
+		if (Mathf.Abs(transform.position.x) > maxDistance)
+			newPosition.x = Mathf.Clamp(newPosition.x, -maxDistance, maxDistance);
+		transform.position = newPosition;
+	}
+}
