48865d085bdb2e6bfb632b15ea626f162d0d20d7
Drew Gingerich <gingerich93@gmail.com>
2018-07-21 13:44:48 -0700

    Make script to scale quad to fit applied texture.

---
+++ Assets/scripts/ScaleToTexture.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(MeshRenderer))]
+public class ScaleToTexture : MonoBehaviour {
+
+	[SerializeField] Material textureMaterial;
+	[SerializeField] int pixPerUnit = 900;
+	new MeshRenderer renderer;
+	
+	void Awake() {
+		renderer = GetComponent<MeshRenderer>();
+	}
+
+	void Update() {
+		Texture texture = renderer.sharedMaterial.mainTexture;
+		Vector2 size = new Vector2(texture.width, texture.height) / pixPerUnit;
+		transform.localScale = (Vector3)size;
+	}
+}
