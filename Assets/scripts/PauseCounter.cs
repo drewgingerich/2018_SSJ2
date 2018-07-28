e2ca7775656ba88a4791f62613bc1cc39570e07b
Drew Gingerich <gingerich93@gmail.com>
2018-07-28 14:08:47 -0700

    Finish pause menu.

---
+++ Assets/scripts/PauseCounter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PauseCounter : MonoBehaviour {
+
+	public UnityEvent OnPause;
+	public UnityEvent OnUnpause;
+	
+	int counter = 0;
+
+	public void Pause() {
+		if (counter == 0)
+			OnPause.Invoke();
+		counter++;
+	}
+
+	public void Unpause() {
+		counter--;
+		if (counter == 0)
+			OnUnpause.Invoke();
+	}
+}
---
+++ Assets/scripts/PauseDisplay.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class PauseDisplay : MonoBehaviour {
+
+	public UnityEvent OnPause;
+	public UnityEvent OnUnpause;
+	
+	[SerializeField] GameObject pauseMenu;
+	[SerializeField] Text goalText;
+
+	bool paused;
+
+	void Start() {
+		pauseMenu.SetActive(false);
+	}
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+			TogglePause();
+	}
+
+	public void TogglePause() {
+		paused = !paused;
+		if (paused)
+			Pause();
+		else
+			Unpause();
+	}
+
+	void Pause() {
+		pauseMenu.SetActive(true);
+		Time.timeScale = 0;
+		OnPause.Invoke();
+	}
+
+	void Unpause() {
+		pauseMenu.SetActive(false);
+		Time.timeScale = 1;
+		OnUnpause.Invoke();
+	}
+
+	public void SetGoalText(string text) {
+		goalText.text = text;
+	}
+}
