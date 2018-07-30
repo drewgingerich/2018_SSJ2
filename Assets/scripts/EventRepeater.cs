cee5833704f2f4bd35ceb1a1aa5ec2ba673e65ee
Drew Gingerich <gingerich93@gmail.com>
2018-07-30 02:47:23 -0700

    Add black screen pause after intro dialogue for dramatic effect.

---
+++ Assets/scripts/EventRepeater.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventRepeater : MonoBehaviour {
+
+	public UnityEvent OnReceive;
+	public UnityEvent OnRepeat;
+
+	[SerializeField] bool delay;
+	[SerializeField] float delayTime;
+
+	public void Receive() {
+		OnReceive.Invoke();
+		StartCoroutine(Delay(delayTime));
+	}
+
+	IEnumerator Delay(float time) {
+		yield return new WaitForSeconds(time);
+		OnRepeat.Invoke();
+	}
+}
