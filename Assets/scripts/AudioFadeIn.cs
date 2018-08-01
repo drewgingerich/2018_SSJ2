1946b7d350aff7358f6f93ac480f7e70b2a4e65c
Drew Gingerich <gingerich93@gmail.com>
2018-08-01 15:29:32 -0700

    Add background talking to intro scene, to set place as a classroom or cafeteria, maybe?

---
+++ Assets/scripts/AudioFadeIn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AudioFadeIn : MonoBehaviour {
+
+	public UnityEvent OnFinish;
+
+	[SerializeField] AudioSource audioSource;
+	[SerializeField] bool onStart = false;
+	[SerializeField] float fadeTime = 1;
+
+	void Start() {
+		if (onStart) {
+			StartCoroutine(FadeIn());
+		}
+	}
+
+	public IEnumerator FadeIn() {
+		float inverseFadeTime = 1 / fadeTime;
+		float progress = 0;
+		audioSource.volume = 0;
+		while (audioSource.volume < 1) {
+			progress += Time.deltaTime * inverseFadeTime;
+			audioSource.volume = Mathf.Lerp(0, 1, progress);
+			yield return null;
+		}
+		OnFinish.Invoke();
+	}
+}
